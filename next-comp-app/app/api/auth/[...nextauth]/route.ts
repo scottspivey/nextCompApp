6412555e75eaccb76eb0a2d6734a3ef641064725
scottspivey <scottspivey@gmail.com>
2025-04-13 14:38:34 -0400

    Changes to be committed: 	new file:   next-comp-app/app/api/auth/[...nextauth]/auth.ts 	deleted:    next-comp-app/app/api/auth/[...nextauth]/authOptions.ts 	modified:   next-comp-app/app/api/auth/[...nextauth]/route.ts 	new file:   next-comp-app/auth.ts 	modified:   next-comp-app/package-lock.json 	modified:   next-comp-app/package.json 	modified:   next-comp-app/prisma/node_modules/@prisma/client/edge.js 	modified:   next-comp-app/prisma/node_modules/@prisma/client/index.js

--- next-comp-app/app/api/auth/[...nextauth]/route.ts
+++ next-comp-app/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,5 @@
-// src/app/api/auth/[...nextauth]/route.ts
+import { handlers } from "@/auth"
 
-import NextAuth from 'next-auth';
-// Import authOptions from your new utility file
-import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'; // Adjust path if needed
+export const { GET, POST } = handlers
 
-// Initialize NextAuth with the imported options
-const handler = NextAuth(authOptions);
-
-// Export only the required handlers for App Router
-export { handler as GET, handler as POST };
-
+export { auth as middleware } from "@/auth"
